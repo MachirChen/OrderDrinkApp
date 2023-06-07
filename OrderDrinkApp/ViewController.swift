e0fffb7f5bc211a73f95fb6f86a77fd138108b90
Machir <machirchen@gmail.com>
2023-06-08 00:08:44 +0800

    Initial Commit

---
+++ OrderDrinkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderDrinkApp
+//
+//  Created by Machir on 2023/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
